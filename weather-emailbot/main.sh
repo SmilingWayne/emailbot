4779dd2d591ac1f46f94984e29c2a8c31a97bd61
xiaoxiao <593302270@qq.com>
2023-02-02 14:31:28 +0800

    add email robot

---
+++ weather-emailbot/main.sh
@@ -0,0 +1,12 @@
+set -eux
+
+CITY=Nanjing
+LANGUAGE="zh-CN"
+UNIT=m
+UA="Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.1 Safari/605.1.15"
+
+curl \
+  -H "Accept-Language: $LANGUAGE" \
+  -H "User-Agent: $UA" \
+  -o result.html \
+  wttr.in/$CITY?format=3\&$UNIT
