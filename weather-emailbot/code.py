2d009703722b39d10d160de9f2172eebb9ed8639
xiaoxiao <593302270@qq.com>
2023-02-08 20:49:24 +0800

    Beautify Weather report

---
+++ weather-emailbot/code.py
@@ -0,0 +1,28 @@
+import requests
+
+
+# NJ：101190101
+# TZ：101191201
+
+#api地址
+
+if __name__ == "__main__":
+    url = 'http://t.weather.sojson.com/api/weather/city/'
+    try:
+        response_NJ = requests.get(url + "101190101")
+        d = response_NJ.json()
+        message = ""
+        if(d['status'] == 200):
+            message += ("城市：" + d["cityInfo"]["parent"] +  d["cityInfo"]["city"] + "\n")
+            message += ("温度：" + d["data"]["forecast"][0]["high"] + "  " + d["data"]["forecast"][0]["low"] + "\n")
+            message += ("天气: " + d["data"]["forecast"][0]["type"] + "\n")
+            if "雨" in d["data"]["forecast"][0]["type"]:
+                message += ("下雨，别忘记带雨伞～" + "\n" )
+            message += "记得微笑😊"
+            # message += ("城市：" + d["cityInfo"]["parent"], d["cityInfo"]["city"] + "\n")
+            # print(message)
+    except Exception:
+        message = "数据载入失败～机器人故障~(BIBI)\n"
+
+    with open("ToSendEmail.txt", 'w', encoding="utf-8") as email:
+        email.writelines(message)
--- weather-emailbot/main.sh
+++ weather-emailbot/main.sh
@@ -1,12 +1,14 @@
 set -eux
 
-CITY=Nanjing
-LANGUAGE="zh-CN"
-UNIT=m
-UA="Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.1 Safari/605.1.15"
+# CITY=Nanjing
+# LANGUAGE="zh-CN"
+# UNIT=m
+# UA="Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.1 Safari/605.1.15"
 
-curl \
-  -H "Accept-Language: $LANGUAGE" \
-  -H "User-Agent: $UA" \
-  -o result.html \
-  wttr.in/$CITY?format=3\&$UNIT
+# curl \
+#   -H "Accept-Language: $LANGUAGE" \
+#   -H "User-Agent: $UA" \
+#   -o result.html \
+#   wttr.in/$CITY?format=3\&$UNIT
+
+python weather-emailbot/code.py
