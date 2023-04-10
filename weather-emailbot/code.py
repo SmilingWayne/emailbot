1762e697b81f5f365fd67cae61d39c1208afbd1a
xiaoxiao <593302270@qq.com>
2023-04-10 08:36:42 +0800

    Modify Contents[Keep alive]

--- weather-emailbot/code.py
+++ weather-emailbot/code.py
@@ -4,7 +4,7 @@ import requests
 # NJ：101190101
 # TZ：101191201
 
-#api地址
+# api地址
 
 if __name__ == "__main__":
     url = 'http://t.weather.sojson.com/api/weather/city/'
@@ -13,12 +13,14 @@ if __name__ == "__main__":
         d = response_NJ.json()
         message = ""
         if(d['status'] == 200):
-            message += ("城市：" + d["cityInfo"]["parent"] +  d["cityInfo"]["city"] + "\n")
-            message += ("温度：" + d["data"]["forecast"][0]["high"] + "  " + d["data"]["forecast"][0]["low"] + "\n")
+            message += ("城市：" + d["cityInfo"]["parent"] +
+                        d["cityInfo"]["city"] + "\n")
+            message += ("温度：" + d["data"]["forecast"][0]["high"] +
+                        "  " + d["data"]["forecast"][0]["low"] + "\n")
             message += ("天气: " + d["data"]["forecast"][0]["type"] + "\n")
             if "雨" in d["data"]["forecast"][0]["type"]:
-                message += ("下雨，别忘记带雨伞～" + "\n" )
-            message += "记得微笑😊"
+                message += ("下雨，别忘记带雨伞～" + "\n")
+            message += "记得微笑😊天天开心～"
             # message += ("城市：" + d["cityInfo"]["parent"], d["cityInfo"]["city"] + "\n")
             # print(message)
     except Exception:
